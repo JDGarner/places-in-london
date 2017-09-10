761c10751edf1bc3dc1affeb10e71c19b10f21e1
Jamie Garner <jamiegarner123@hotmail.co.uk>
2017-09-10 17:08:29 +0100

    add snapshot test

--- src/components/place/place.test.js
+++ src/components/place/place.test.js
@@ -1,5 +1,12 @@
-describe('Addition', () => {
-  it('knows that 2 and 2 make 4', () => {
-    expect(2 + 2).toBe(4);
-  });
+import React from 'react';
+import Place from './place';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+test('<Place /> renders correctly', () => {
+  const tree = shallow(
+    <Place name="Park"/>
+  );
+
+  expect(toJson(tree)).toMatchSnapshot();
 });
